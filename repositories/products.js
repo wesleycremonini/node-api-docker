b5a13e670f62a02584c3385c48a9ef3b51f513a0
wesleycremonini <wesley18.cre@gmail.com>
2022-06-30 16:15:13 -0300

    adiciona sort

--- repositories/products.js
+++ repositories/products.js
@@ -1,9 +1,7 @@
-
 const Product = require('../models/product') //model
 
-
 async function getAll(query) {
-	return await Product.findAll({ where: query })
+	return await Product.findAll(query)
 }
 
 module.exports = { getAll }
--- services/products.js
+++ services/products.js
@@ -3,16 +3,32 @@ const Product = require('../models/product') //model
 const ProductAttributes = Object.keys(Product.getAttributes()) // parametros existentes no model
 const { Op } = require('sequelize') // operator
 
-async function getAll(query) {
-  for (const field in query) {
-    if (!ProductAttributes.includes(field)) {
-			delete query[field]
-		} else {
-      query[field] = { [Op.regexp]: query[field] }
-    }
+async function getAll(data) {
+	const query = {}
+
+	if (data.sort) {
+    query.order = []
+		const sort = data.sort.split(',')
+		sort.forEach((field) => {
+			query.order.push([
+				field.replace('-', ''),
+				field.startsWith('-') ? 'DESC' : 'ASC',
+			])
+		})
+		delete data.sort
 	}
 
-  return await repository.getAll(query)
+	for (const field in data) {
+		if (!ProductAttributes.includes(field)) {
+			delete data[field]
+		} else if (data[field]) {
+			data[field] = { [Op.regexp]: data[field] }
+		}
+	}
+
+	query.where = data
+
+	return await repository.getAll(query)
 }
 
 module.exports = { getAll }
