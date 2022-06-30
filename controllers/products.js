aecac06a907b7ffa94070a2ff777c31e82623408
wesleycremonini <wesley18.cre@gmail.com>
2022-06-30 14:14:42 -0300

    adiciona count nos resultados

--- controllers/products.js
+++ controllers/products.js
@@ -3,10 +3,10 @@ const service = require('../services/products')
 /*
  * Retorna todos os produtos da query
  * Se o parâmetro não existir ele é ignorado
- * Se o parametro search existir, todos os outros são ignorados
  */
-function getAll(req, res) {
-	res.status(200).json(service.getAll(req))
+async function getAll(req, res) {
+	const products = await service.getAll(req.query)
+	res.status(200).json({ count: products.length, products: products })
 }
 
 module.exports = {
