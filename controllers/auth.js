5d7b2e8c7c50a9ac7d812c237926c4ecae1bfc6a
wesleycremonini <wesley18.cre@gmail.com>
2022-07-01 10:49:02 -0300

    last

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken')
 const service = require('../services/auth')
 
 // errors
@@ -10,7 +11,19 @@ async function login(req, res) {
 		throw new error.BadRequest('Por favor, informe usuário e senha.')
 	}
 
-	res.send(`login`)
+	// O ID TEM Q SER DA DB
+	const id = new Date().getDate()
+
+	const token = jwt.sign(
+		{
+			id,
+			username,
+		},
+		process.env.JWT_SECRET,
+		{ expiresIn: '30d' }
+	)
+
+	res.status(200).json({ msg: 'Usuário logado', token })
 }
 
 module.exports = { login }
